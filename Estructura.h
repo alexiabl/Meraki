510e4fe45852ef8f506482d6e947cd4aa626380b
isatilla <mariaisabel-sbf@hotmail.com>
2018-06-06 02:32:13 -0600

    Update Estructura.h

--- Estructura.h
+++ Estructura.h
@@ -34,48 +34,33 @@ public:
         arreglo.clear(); //limpia el vector
     };
 
-       /* NOTA: como es un vector de listas, C++ no permite realizar estas inserciones
-       (los nombres de las reglas) en el vector como tal, sin embargo, como nosotros vamos
-       a insertar los tokens manualmente dentro de las listas que corresponden a cada regla no
-       hace falta. Esto significa que la lista en la posición 0 del vector corresponde a la gramática
-       de asignación y así sucesivamente. 
+    /* NOTA: como es un vector de listas, C++ no permite realizar estas inserciones
+    (los nombres de las reglas) en el vector como tal, sin embargo, como nosotros vamos
+    a insertar los tokens manualmente dentro de las listas que corresponden a cada regla no
+    hace falta. Esto significa que la lista en la posición 0 del vector corresponde a la gramática
+    de main y así sucesivamente. Sabiendo esto haremos insert("token1",1) para insertar
+    "token1" en la lista de LLamado (que es la lista 1).
+    */
 
-       Especificación de las listas de reglas en el vector:
-       // arreglo[0]="Asignacion";
-       // arreglo[1]="Devuelva";
-       // arreglo[2]="Declaracion";
-       // arreglo[3]="Iteracion";
-       // arreglo[4]="Comparacion";
-       // arreglo[5]="Imprimir";
-       // arreglo[6]="Haga";
-       // arreglo[7]="Mientras";
-       // arreglo[8]="Si";
-       // arreglo[9]="Funcion";
-       // arreglo[10]="BloqueCodigo";
-       // arreglo[11]="BloqueMain";
-       // arreglo[12]="Main";
-       // arreglo[13]="Bloque";
-       // arreglo[14]="Meraki";
-       */
-
-    int search(const T& token,int pos) //token específico en la lista de una regla específica
+    int search(const T& token,int pos,int aparicion) //token específico en la lista de una regla específica
     {
-        int contador=0;
-        typename list<T>::iterator it; //para accesar a la posición del arreglo correspondiente
-        it = arreglo[pos].begin(); //me posiciono en el primer elemento de la lista
+        typename list<T>::iterator iter;
 
-        while(it != arreglo[pos].end() && *it != token) //busco en cada nodo de la lista correspondiente
+        iter = arreglo[pos].begin(); //me posiciono en el primer elemento de la lista
+        if(aparicion!=0) std::advance(iter,aparicion); //track de última aparición de la regla en la misma lista
+
+        while(iter != arreglo[pos].end() && *iter != token) //busco en cada nodo de la lista correspondiente
         {
             //se detiene al llegar al final de la lista o encontrar token
-            std::advance(it,1); //cada que no se encuentre el token, avanzo un nodo
-            contador+=1;
+            std::advance(iter,1); //cada que no se encuentre el token, avanzo un nodo
+            aparicion+=1;
         }
         //se llega al elemento deseado, si existe
 
-        if(*it == token)
+        if(*iter == token)
         {
             // std::cout<< "token encontrado " << *it << endl;
-            return contador;
+            return aparicion;
         }
         else //si se llegó al final de la lista y ninguno es el token
         {
@@ -91,16 +76,16 @@ public:
     };
     // Inserta el elemento en la lista de una regla
 
-    void actualizarPila(const T& tokenI, const T& tokenF, int pos)
+    void actualizarPila(const T& tokenI, const T& tokenF, int regla, int aparicion)
     {
         PilaToken tok; //objeto para insertar a la pila, es uno por regla
-        tok.tipo = pos; //eventual traducción?
+        tok.tipo = regla; //ocupamos una forma de traducir la posición a la regla que es, como guardar las reglas en un vector por aparte
 
-        //por ahora dejo esto así, le damos el token inicial y final de la regla que tenemos en la lista
+        //le damos el token inicial y final de la regla que tenemos en la lista
         //y vemos dónde empieza y termina esa regla específica
 
-        tok.posicionInicial = search(tokenI,pos);
-        tok.posicionFinal = search(tokenF,pos);
+        tok.posicionInicial = search(tokenI,regla,aparicion);
+        tok.posicionFinal = search(tokenF,regla,aparicion);
         pila.push(tok); //insertar en la pila el objeto con la info
     }
 
@@ -109,16 +94,15 @@ public:
         for (int i = 0; i < numEntradas; i++) //acceso a cada una de las listas
         {
             std::cout << "lista #" << i << ": ";
-            for (typename list<T>::iterator it = arreglo[i].begin(); it != arreglo[i].end(); it++)
+            for (typename list<T>::iterator it2 = arreglo[i].begin(); it2 != arreglo[i].end(); it2++)
             {
-                std::cout << *it << " "; //recorrido individual de cada nodo de la lista
+                std::cout << *it2 << " "; //recorrido individual de cada nodo de la lista
             }
             std::cout << std::endl;
         }
-        
         std::cout << std::endl;
         cout << "pop pila" << endl;
-        
+
         while (!pila.empty())
         {
             PilaToken obj = pila.front();
@@ -135,8 +119,8 @@ private:
     vector<list<T> > arreglo;
     // El arreglo es un vector de listas de STL
     queue<PilaToken> pila;
-    // Arreglo con la definicion de las posiciones del arreglo de tipos o reglas
-    string reglas[15] = {"Asignacion", "Devuelva","Declaracion","Iteracion","Comparacion","Imprimir",
-    "Haga","Mientras","Si","Funcion","BloqueCodigo","BloqueMain","Main","Bloque","Meraki"};
+    string reglas [15] = {"Asignacion", "Devuelva","Declaracion","Iteracion","Comparacion","Imprimir",
+                          "Haga","Mientras","Si","Funcion","BloqueCodigo","BloqueMain","Main","Bloque","Meraki"
+                         };
 };
 #endif
